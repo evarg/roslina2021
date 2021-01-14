a2f1cbfbe63f825842f4b73d468d5c17f7d0df7f
Grave Evarg <grave432@gmail.com>
2021-01-14 12:13:31 +0100

    zainstalowany laravel

---
+++ app/Http/Controllers/NasionoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nasiono;
+use Illuminate\Http\Request;
+
+class NasionoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nasiono  $nasiono
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Nasiono $nasiono)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nasiono  $nasiono
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Nasiono $nasiono)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Nasiono  $nasiono
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Nasiono $nasiono)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nasiono  $nasiono
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Nasiono $nasiono)
+    {
+        //
+    }
+}
---
+++ database/seeders/SeedSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class SeedSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
